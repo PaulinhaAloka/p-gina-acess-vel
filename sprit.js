0fe7f689466c1789e044acdf27079bb5c248f81a
UM ELEFAANTE INCOMODA MUITA GENTE <159184945+PaulinhaAloka@users.noreply.github.com>
2025-03-18 11:20:12 -0300

    17/03

---
+++ sprit.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+const aumentafonteBotao = document.getElementById('aumentar-fonte');
+const diminuifonteBotao = document.getElementById('diminuir-fonte');
+
+let tamanhoAtualFonte = 1;
+
+aumentafonteBotao. addEventListener('click', function(){
+
+}
+)
+
+}
+)
